aa3adc7061a28ca3ecbe565638af7a5eaecb5f65
oooiiicc <oooiiicc@users.noreply.github.com>
2026-07-20 16:02:03 +0800

    feat: add speed.oooiii.cc monitor

--- uptime.config.ts
+++ uptime.config.ts
@@ -34,6 +34,14 @@ const workerConfig: WorkerConfig = {
       expectedCodes: [200],
       timeout: 15000,
     },
+    {
+      id: 'speed-oooiii',
+      name: 'speed.oooiii.cc',
+      method: 'GET',
+      target: 'https://speed.oooiii.cc',
+      expectedCodes: [200, 301, 302],
+      timeout: 15000,
+    },
   ],
 }
 
